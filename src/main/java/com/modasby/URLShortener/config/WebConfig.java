2e48da51f7a9462db0ea3da91d79209b63abb412
MoDasby <giullianomendes03>
2022-06-18 21:06:00 -0300

    cors config

---
+++ src/main/java/com/modasby/URLShortener/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.modasby.URLShortener.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
